7d9d6d5f4b1fb76140447fa466df7e0b2781ec75
Jo Daiber <jodaiber@gmx.de>
2011-02-12 22:26:40 +0100

    Added reducer.

--- annotated_wikiextractor/mapreduce/mapper.py
+++ annotated_wikiextractor/mapreduce/mapper.py
@@ -1,16 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import sys
-import os
-
-#Add the folder containing annotated_wikiextractor to the PYTHON_PATH, so
-#it can be executed in hadoop 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../..")))
-
-#Import annotated_wikiextractor from the path added in the last step
-import annotated_wikiextractor
-from annotated_wikiextractor.annotated_wikiextractor import AnnotatedWikiExtractor
-
-#Use the standard AnnotatedWikiExtractor
-wiki_extractor = AnnotatedWikiExtractor()
+import pickle
 
 page = []
 for line in sys.stdin:
@@ -18,8 +9,6 @@ for line in sys.stdin:
     if line == '<page>':
         page = []
     elif line == '</page>':
-        wiki_document = annotated_wikiextractor.wikiextractor.extract_document(page)
-        annotated_wiki_document = wiki_extractor.extract(wiki_document)
-        print "%s\t%s" % (annotated_wiki_document["url"].replace(annotated_wikiextractor.wikiextractor.prefix, ""), annotated_wiki_document)
+        print pickle.dumps(page)
     else:
         page.append(line)
---
+++ annotated_wikiextractor/mapreduce/reducer.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import pickle
+import sys
+import os
+
+#Add the folder containing annotated_wikiextractor to the PYTHON_PATH, so
+#it can be executed in hadoop 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../..")))
+
+#Import annotated_wikiextractor from the path added in the last step
+import annotated_wikiextractor
+from annotated_wikiextractor.annotated_wikiextractor import AnnotatedWikiExtractor
+
+#Use the standard AnnotatedWikiExtractor
+wiki_extractor = AnnotatedWikiExtractor()
+
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    page = pickle.loads(line)
+    wiki_document = annotated_wikiextractor.wikiextractor.extract_document(page)
+    annotated_wiki_document = wiki_extractor.extract(wiki_document)
+    print "%s\t%s" % (annotated_wiki_document["url"].replace(annotated_wikiextractor.wikiextractor.prefix, ""), annotated_wiki_document)
